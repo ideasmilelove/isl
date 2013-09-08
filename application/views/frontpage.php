63827e88a24bdab21cf2808dc18f4bb0fc0da2cc
Zaihao <zaihao90@hotmail.com>
2013-09-08 19:35:20 +0800

    doing the layout
    still adding the css for the frontpage.

--- application/views/frontpage.php
+++ application/views/frontpage.php
@@ -1,6 +1,6 @@
 <html>
  <head>
- <link rel="stylesheet" type="text/css" href="frontpage.css">
+ <link rel="stylesheet" type="text/css" href="media/css/frontpage.css">
   <title>PHP Test</title>
  </head>
  <body>	
@@ -10,8 +10,11 @@
 	 <div class="loginbar">
 	  <?php echo '<p>login</p>'; ?> 
 	 </div>
-	 <div class="salespage">
+	 <div class="salesbar">
 	  <?php echo '<p>lastes sale</p>'; ?> 
+	 </div>
+	 <div class="footerbar">
+	  <?php echo '<p>footer</p>'; ?> 
 	 </div>
  </body>
 </html>
