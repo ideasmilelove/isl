f326a83c98456a8d2b6f56fb9028687f5934aa1c
Zaihao <zaihao90@hotmail.com>
2013-09-09 00:02:25 +0800

    small bit change

--- application/views/frontpage.php
+++ application/views/frontpage.php
@@ -3,11 +3,10 @@
  <link rel="stylesheet" type="text/css" href="media/css/frontpage.css">
   <title>PHP Test</title>
  </head>
- <body>	
+ <body id="frontbody">	
 	 <div class="titlebar">
 	  <?php echo '<p>title</p>'; ?> 
-	 </div>
-	 <div class="loginbar">
+	 </div><div class="loginbar">
 	  <?php echo '<p>login</p>'; ?> 
 	 </div>
 	 <div class="salesbar">
