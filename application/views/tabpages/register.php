6afa460498342011e086d977f54816da9403a8bb
Zaihao <zaihao90@hotmail.com>
2013-09-12 20:47:03 +0800

    added retype pass in register.php

--- application/views/tabpages/register.php
+++ application/views/tabpages/register.php
@@ -2,17 +2,20 @@
 
 echo Form::open('register');
 echo Form::input('regfullnametxt',NULL,array( 'class' => 'txtbox',
-										'id' => 'fullnametxt',
+										'id' => 'regfullnametxt',
 										'placeholder' => '  Full Name'))
 //echo Form::label('emaillbl', 'Email Address'); 
 echo Form::input('regemailtxt',NULL,array( 'class' => 'txtbox',
-										'id' => 'emailtxt',
+										'id' => 'regemailtxt',
 										'placeholder' => '  Email')); 
  //echo Form::label('passwordlbl', 'Password');
 
 echo Form::password('regpassword',NULL,array(	'class' => 'txtbox',
-                              				'id' => 'passtxt',
+                              				'id' => 'regpasstxt',
                               				'placeholder' => '  Password')); 
+echo Form::password('regretypepassword',NULL,array(	'class' => 'txtbox',
+                              				'id' => 'regretypepasstxt',
+                              				'placeholder' => '  Re-Type Password')); 
 
 echo Form::submit('save', '  Register  ',array('class' => 'registerbtn blue',
                               				'id' => 'registerbtn blue')); 
