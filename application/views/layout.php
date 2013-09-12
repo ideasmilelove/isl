afd912b635e39c162d074db6c6061ed4ae20752b
STAN <ideasmilelove@gmail.com>
2013-09-12 18:45:13 +0800

    code standardize and clean up
    dont use spacebar to indent

--- application/views/layout.php
+++ application/views/layout.php
@@ -1,29 +1,38 @@
 <html>
- <head>
-  <link rel="stylesheet" type="text/css" href="media/css/normalize.css">
- <link rel="stylesheet" type="text/css" href="media/css/layout.css">
-  <link rel="stylesheet" type="text/css" href="media/css/logoandquote.css">
-  <link rel="stylesheet" type="text/css" href="media/css/login.css">
-  <title><?php echo $title?></title>
- </head>
- <body id="body">	
- 	<div id="bodyheader">
- 		<div id="headerwrapper">
-			 <div id="title">
-			 <?php echo Form::image(NULL, NULL, array('src' => $logo,
-														'id' => 'logo')); ?>
-			 </div><div id="login">
-			  <?php echo $loginandprofileform; ?> 
-			 </div>
-		 </div>
-	 </div>
-	 <div id="contentwrapper">
-	 	<div id="content">
-	  		<?php echo $contentStuff; ?>
-	  	</div>
-	 </div>
-	 <div id="footer">
-	  <?php echo $copyright; ?> 
-	 </div>
- </body>
+	<head>
+		<!-- STYLES -->
+	 	<link rel="stylesheet" type="text/css" href="media/css/normalize.css">
+		<link rel="stylesheet" type="text/css" href="media/css/layout.css">
+	 	<link rel="stylesheet" type="text/css" href="media/css/logoandquote.css">
+		<link rel="stylesheet" type="text/css" href="media/css/login.css">
+
+		<!-- SCRIPTS -->
+
+	  	<title><?php echo $title?></title>
+ 	</head>
+
+ 	<body>	
+
+ 		<!--.........................................HEAD -->
+ 		<div id="bodyheader">
+ 			<div id="headerwrapper">
+				<div id="title">
+					<?php echo Form::image(NULL, NULL, array('src' => $logo,
+															 'id' => 'logo')); ?>
+				</div>
+
+				<!-- login.php -->
+				<div id="login"><?php echo $loginandprofileform; ?></div>
+			</div>
+	 	</div>
+
+		<!--.........................................CONTENT -->
+		<div id="contentwrapper">
+	 		<div id="content"><?php echo $contentStuff; ?></div>
+		</div>
+
+	 	<!--.........................................FOOT -->
+	 	<div id="footer"><?php echo $copyright; ?></div>
+
+ 	</body>
 </html>
