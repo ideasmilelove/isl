d0128b22bd6b6460041676728793314fa51dbd80
Zaihao <zaihao90@hotmail.com>
2013-09-17 19:53:38 +0800

    aedit search css and class name

--- application/views/search.php
+++ application/views/search.php
@@ -4,12 +4,12 @@ Form::open('login')
 .
 	Form::input('searchtxt',NULL,
 		array(	'class' => 'txtbox rad5',
-				'id' => 'searchtxt',
+				'id' => 'txtsearch',
 				'placeholder' => 'Search'))
 	.
-	Form::submit('save', 'Search',
-		array(	'class' => 'searchbtn_blue rad5 rad15hover',
-				'id' => 'search_btn'))
+	Form::submit('save', 'Q',
+		array(	'class' => 'btn_blue rad15 rad5hover',
+				'id' => 'btn_search'))
 .
 Form::close();
 ?>
