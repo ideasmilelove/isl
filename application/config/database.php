acf5e602d1d67044fee60b670db91a781e493a81
Zaihao <zaihao90@hotmail.com>
2013-09-19 19:37:02 +0800

    added default sql script and database config

---
+++ application/config/database.php
@@ -0,0 +1,19 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+return array(
+    //'title' => 'Our Shiny Website',
+    //'analytics' => FALSE, // analytics code goes here, set to FALSE to disable
+	 'default' => array
+    (
+        'type'       => 'MySQL',
+        'connection' => array(
+            'hostname'   => 'localhost',
+            'username'   => 'root',
+            'password'   => '',
+            'persistent' => FALSE,
+            'database'   => 'isl',
+        ),
+        'table_prefix' => '',
+        'charset'      => 'utf8',
+        'profiling'    => TRUE,
+    ),
+);
